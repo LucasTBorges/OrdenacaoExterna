f233f338b752d8ac758a5638cfc6db42c093ba3d
Lucas Teixeira Borges <lucastborges@gmail.com>
2024-07-19 16:43:47 -0300

    heapcascata agora se comporta como uma heap de verdade

--- CascataPackage/HeapCascata.py
+++ CascataPackage/HeapCascata.py
@@ -7,24 +7,52 @@ class HeapCascata: #TODO: implementar como heap mínima
         if self.isFull:
             raise IndexError(f"Houve a tentativa de adicionar o elemento ({elemento}) a uma memória cheia")
         record = {"value":elemento, "file":fileNum}
-        if len(self._memory) == 0:
-            self._memory.append(record)
-            return
-        for i in range(len(self._memory)):
-            if self._memory[i]["value"] > elemento:
-                self._memory.insert(i, record)
-                return
         self._memory.append(record)
+        self.heapifyUp(len(self._memory)-1)
         return
 
     def dequeue(self)->int:
-        return self._memory.pop(0)
+        if self.isEmpty:
+            raise IndexError("Houve a tentativa de remover um elemento de uma memória vazia")
+        record = self._memory[0]
+        self._memory[0] = self._memory[-1]
+        self._memory.pop()
+        self.heapifyDown(0)
+        return record
     
     def __str__(self) -> str:
         return "(Valor, Origem): {" + " ".join([f'({x["value"]}, {x["file"]})' for x in self._memory]) + "}"
     
     def __repr__(self) -> str:
         return self.__str__()
+    
+    def heapifyDown(self, index:int)->None:
+        left = HeapCascata.leftChild(index)
+        right = HeapCascata.rightChild(index)
+        smallest = index
+        if left < len(self._memory) and self._memory[left]["value"] < self._memory[smallest]["value"]:
+            smallest = left
+        if right < len(self._memory) and self._memory[right]["value"] < self._memory[smallest]["value"]:
+            smallest = right
+        if smallest != index:
+            self._memory[index], self._memory[smallest] = self._memory[smallest], self._memory[index]
+            self.heapifyDown(smallest)
+
+    def heapifyUp(self, index):
+        parent  = HeapCascata.parent(index)
+        if index > 0 and self._memory[index]["value"] < self._memory[parent]["value"]:
+            self._memory[index], self._memory[parent] = self._memory[parent], self._memory[index]
+            self.heapifyUp(parent)
+    
+    @staticmethod
+    def parent(index:int)->int:
+        return (index-1)//2
+    @staticmethod
+    def leftChild(index:int)->int:
+        return 2*index + 1
+    @staticmethod
+    def rightChild(index:int)->int:
+        return 2*index + 2
 
     @property
     def isFull(self)->bool:
