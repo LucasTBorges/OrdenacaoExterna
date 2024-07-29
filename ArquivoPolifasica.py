5c22334f45506d050808e40728d24a8dbae201cd
joshua rafael <rafaeloliveira@MacBook-Air-de-joshua.local>
2024-07-29 18:48:34 -0300

    Configurações e funções básicas

---
+++ ArquivoPolifasica.py
@@ -0,0 +1,55 @@
+class ArquivoPolifasica:
+    def __init__(self, lists: list = []) -> None:
+        self._sequencias = lists
+        self._writeOps = 0
+
+    @property
+    def isEmpty(self)->bool:
+        return len(self._sequencias) == 0 or (len(self._sequencias) == 1 and len(self._sequencias[0]) == 0)
+
+    @property
+    def sequencias(self)->list:
+        return self._sequencias
+
+    @property
+    def qtdRegistros(self)->int:
+        n = 0
+        for seq in self._sequencias:
+            n += len(seq)
+        return n
+    
+    @property
+    def qtdSequencias(self)->int:
+        n=0
+        for seq in self._sequencias:
+            if len(seq) > 0:
+                n += 1
+        return n
+
+    @property
+    def writeOps(self)->int:
+        return self._writeOps
+
+    def appendElemento(self, value:int):
+        if len(self._sequencias)>0:
+            self._sequencias[-1].append(value)
+            self._writeOps += 1
+        else:
+            self._sequencias.append([value])
+            self._writeOps += 1
+
+    def appendSequencia(self, lista:list):
+        self._sequencias.append(lista)
+        self._writeOps += len(lista)
+
+    # def congela(self)->None:
+    #     if self._congelado:
+    #         raise ValueError("Tentativa de congelar arquivo já congelado")
+    #     self._congelado = True
+
+    # def descongela(self)->None:
+    #     if not self._congelado:
+    #         raise ValueError("Tentativa de descongelar arquivo não congelado")
+    #     self._congelado = False
+
+    
---
+++ Polifasica.py
@@ -0,0 +1,64 @@
+from ArquivoPolifasica import ArquivoPolifasica
+
+class Polifasica:
+    def __init__(self, k: int, arquivos: list): 
+        # k = numero max de arquivos abertos concomitantemente
+        self.k = k
+        self.arquivos = arquivos
+
+    def __str__(self)->str:
+        return str([arq.sequencias for arq in self.arquivos])
+
+    @property
+    def completo(self):
+        arquivos = [arq for arq in self.arquivos if not arq.isEmpty]
+        return len(arquivos) == 1 and len(arquivos[0].sequencias) == 1
+
+    def ordenarSequencias(self, sequencias: list):
+        new_ordered_list = []
+        index = [0] * len(sequencias)
+
+        while any(index[i] < len(sequencias[i]) for i in range(len(sequencias))):
+            min_val = float('inf')
+            min_idx = None
+
+            for i in range(len(sequencias)):
+                if index[i] < len(sequencias[i]) and sequencias[i][index[i]] < min_val:
+                    min_val = sequencias[i][index[i]]
+                    min_idx = i
+
+            new_ordered_list.append(min_val)
+            index[min_idx] += 1
+
+        return new_ordered_list
+
+        
+    def polifasear(self):
+        while not self.completo:
+            target_file = None
+            for file in self.arquivos:
+                if file.isEmpty:
+                    target_file = file
+            if target_file is None:
+                raise Exception("Tentativa de executar cascata falhou pois todos os arquivos estavam ocupados")
+
+            arquivos = [arq for arq in self.arquivos if not arq.isEmpty and arq != target_file]
+            while (all([not arq.isEmpty for arq in arquivos])):
+                sequencias = [arq.sequencias[0] for arq in arquivos]
+                sequencia_ordenada = self.ordenarSequencias(sequencias)
+                target_file.appendSequencia(sequencia_ordenada)
+                for arq in self.arquivos:
+                    if arq != target_file and len(arq.sequencias) > 0:
+                        print('Popando arquivos...s')
+                        arq.sequencias.pop(0)
+
+                arqs = [arq.sequencias for arq in arquivos]
+                print('arquivos: ' + str(arqs))
+                arqs = [arq.sequencias for arq in self.arquivos]
+                print('self.arquivos: ' + str(arqs))
+
+
+
+
+
+    
