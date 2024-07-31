bfa6b70f1a97c307bc7c639648c599829b2dba7a
Lucas Teixeira Borges <lucastborges@gmail.com>
2024-07-30 22:44:06 -0300

    Deletado OrdenadorCascata

--- CascataPackage/OrdenadorCascata.py
+++
@@ -1,8 +0,0 @@
-from CascataPackage.SequenciaCascata import SequenciaCascata
-from CascataPackage.ArquivoCascata import ArquivoCascata
-from CascataPackage.Cascata import Cascata
-
-class OrdenadorCascata:
-    def __init__(self, input:list[int], ramSize: int)->None:
-        self._input = input
-        self._ramSize = ramSize
