cd1262f6ad1e57c2ab7d07b99c725508aaa58946
joshua rafael <rafaeloliveira@MacBook-Air-de-joshua.local>
2024-08-01 18:12:34 -0300

    Renomear variaveis

--- PolifasicaPackage/Polifasica.py
+++ PolifasicaPackage/Polifasica.py
@@ -38,26 +38,25 @@ class Polifasica:
     def polifasear(self):
         while not self.completo:
             target_file = None
-            for file in self.arquivos:
+            for file in self._arquivos:
                 if file.isEmpty:
                     target_file = file
             if target_file is None:
                 raise Exception("Tentativa de executar cascata falhou pois todos os arquivos estavam ocupados")
 
-            arquivos = [arq for arq in self.arquivos if not arq.isEmpty and arq != target_file]
+            arquivos = [arq for arq in self._arquivos if not arq.isEmpty and arq != target_file]
             while (all([not arq.isEmpty for arq in arquivos])):
                 sequencias = [arq.sequencias[0] for arq in arquivos]
                 sequencia_ordenada = self.ordenarSequencias(sequencias)
                 target_file.appendSequencia(sequencia_ordenada)
-                for arq in self.arquivos:
+                for arq in self._arquivos:
                     if arq != target_file and len(arq.sequencias) > 0:
-                        print('Popando arquivos...s')
                         arq.sequencias.pop(0)
 
-                arqs = [arq.sequencias for arq in arquivos]
-                print('arquivos: ' + str(arqs))
-                arqs = [arq.sequencias for arq in self.arquivos]
-                print('self.arquivos: ' + str(arqs))
+                # arqs = [arq.sequencias for arq in arquivos]
+                # print('arquivos: ' + str(arqs))
+                # arqs = [arq.sequencias for arq in self._arquivos]
+                # print('self._arquivos: ' + str(arqs))
 
 
 
