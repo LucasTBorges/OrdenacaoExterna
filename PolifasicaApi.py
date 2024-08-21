35a1580a4f2b0d14364dc0bb122df4676ff34482
Luisa <luisacoelho@ufba.br>
2024-08-21 20:51:28 -0300

    Colocar os parametros na mesma ordem da API do Cascata

--- PolifasicaApi.py
+++ PolifasicaApi.py
@@ -2,17 +2,17 @@ from PolifasicaPackage.Polifasica import Polifasica
 from DadosExecucao import DadosExecucao
 
 class PolifasicaApi:
-    def _run(self, ramSize:int, qtdArquivos:int, seqsInic: list[list[int]], printResult:bool, beta_test:bool = False)->DadosExecucao:
+    def _run(self, seqsInic: list[list[int]], ramSize:int, qtdArquivos:int, printResult:bool, beta_test:bool = False)->DadosExecucao:
         polifasica = Polifasica(ramSize, qtdArquivos, seqsInic)
         polifasica.run(beta_test)
         if printResult:
             print(polifasica.output)
         return polifasica.dadosExecucao
     
-    def run(self, ramSize:int, qtdArquivos:int, seqsInic: list[list[int]], beta_test:bool = False)->DadosExecucao:
+    def run(self, seqsInic: list[list[int]], ramSize:int, qtdArquivos:int, beta_test:bool = False)->DadosExecucao:
         return self._run(ramSize, qtdArquivos, seqsInic, False, beta_test)
     
-    def runAndPrint(self, ramSize:int, qtdArquivos:int, seqsInic: list[list[int]])->DadosExecucao: #Ordena e imprime o resultado
+    def runAndPrint(self, seqsInic: list[list[int]], ramSize:int, qtdArquivos:int)->DadosExecucao: #Ordena e imprime o resultado
         return self._run(ramSize, qtdArquivos, seqsInic, True)
     
     def runFromDados(self, dados: DadosExecucao)->DadosExecucao:
